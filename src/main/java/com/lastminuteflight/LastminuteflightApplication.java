4c4ad9477b17504e84e1f1db0c3d0670de7ca57b
guswls78963210 <guswls78963210@gmail.com>
2024-05-08 20:52:54 +0900

    [Feat - #1] 프로젝트 초기 구성
    프로젝트 초기 구성을 진행
    빌드 툴은 Gradle 을 사용하였고, 기초적인 의존성을 추가함
    (Spring - web, data jpa, validation, DB - mysql connector, etc - lombok)

---
+++ src/main/java/com/lastminuteflight/LastminuteflightApplication.java
@@ -0,0 +1,13 @@
+package com.lastminuteflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastminuteflightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastminuteflightApplication.class, args);
+	}
+
+}
