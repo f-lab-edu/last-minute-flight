f1e60d2412721cc51d8b2339fe62d9993a423f40
guswls78963210 <guswls78963210@gmail.com>
2024-05-27 03:33:35 +0900

    [Feature - #3] Api 모듈에서 사용할 Request, Response 객체와 Mapper 생성
    Api 모듈에 해당 클래스들을 생성한다면,
    하위 모듈인 Application 모듈에서 사용하지 못하기 때문에 Application 모듈에 생성함

---
+++ flight-application/src/main/java/com/flight/application/dto/TestCreateRequest.java
@@ -0,0 +1,4 @@
+package com.flight.application.dto;
+
+public record TestCreateRequest(String name) {
+}
---
+++ flight-application/src/main/java/com/flight/application/dto/TestCreateResponse.java
@@ -0,0 +1,4 @@
+package com.flight.application.dto;
+
+public record TestCreateResponse(Long id, String name) {
+}
