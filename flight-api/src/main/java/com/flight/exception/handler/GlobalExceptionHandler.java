a98fcd92d4fd4f4e8681909afe85fcb6cff8ef6c
guswls78963210 <guswls78963210@gmail.com>
2024-06-07 00:28:14 +0900

    [Feat - #5] 애플리케이션의 예외를 잡아줄 핸들러 작성
    현재 발생할 수 있는 입력값 관련 오류 설정을 추가함

---
+++ flight-api/src/main/java/com/flight/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.flight.exception.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.flight.common.exception.BadRequestException;
+import com.flight.common.exception.DuplicatedFieldException;
+import com.flight.common.exception.ErrorCode;
+import com.flight.dto.ApiResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<ApiResponse<Void>> badRequestException(BadRequestException e) {
+
+		ErrorCode errorCode = e.getErrorCode();
+		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
+
+		return ResponseEntity.status(errorCode.getHttpStatusCode())
+			.body(response);
+	}
+
+	@ExceptionHandler(DuplicatedFieldException.class)
+	public ResponseEntity<ApiResponse<Void>> duplicatedFieldException(DuplicatedFieldException e) {
+
+		ErrorCode errorCode = e.getErrorCode();
+		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
+
+		return ResponseEntity.status(errorCode.getHttpStatusCode())
+			.body(response);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiResponse<Void>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+		ErrorCode errorCode = ErrorCode.INVALID_INPUT_PATTERN;
+		String clientMessage = e.getAllErrors().get(0).getDefaultMessage();
+		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), clientMessage);
+
+		return ResponseEntity.status(errorCode.getHttpStatusCode())
+			.body(response);
+	}
+}
