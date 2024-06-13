4087cd49bdabfa4688d0f076f76915c3d392cb8d
guswls78963210 <guswls78963210@gmail.com>
2024-06-13 23:12:57 +0900

    [Refactor - #5] 예외 핸들링 통합
    기존에 분리되어 있던 예외 핸들링 메서드가 같은 코드로 작성되었기 때문에,
    추상화된 타입으로 한 번에 처리할 수 있도록 변경

--- flight-api/src/main/java/com/flight/exception/handler/GlobalExceptionHandler.java
+++ flight-api/src/main/java/com/flight/exception/handler/GlobalExceptionHandler.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.flight.common.exception.BadRequestException;
-import com.flight.common.exception.DuplicatedFieldException;
 import com.flight.common.exception.ErrorCode;
-import com.flight.common.exception.PasswordNotMatchException;
-import com.flight.common.exception.ResourceNotFoundException;
-import com.flight.common.exception.UnAuthenticatedException;
 import com.flight.dto.ApiResponse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,46 +17,6 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(BadRequestException.class)
 	public ResponseEntity<ApiResponse<Void>> badRequestException(BadRequestException e) {
-
-		ErrorCode errorCode = e.getErrorCode();
-		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
-
-		return ResponseEntity.status(errorCode.getHttpStatusCode())
-			.body(response);
-	}
-
-	@ExceptionHandler(DuplicatedFieldException.class)
-	public ResponseEntity<ApiResponse<Void>> duplicatedFieldException(DuplicatedFieldException e) {
-
-		ErrorCode errorCode = e.getErrorCode();
-		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
-
-		return ResponseEntity.status(errorCode.getHttpStatusCode())
-			.body(response);
-	}
-
-	@ExceptionHandler(PasswordNotMatchException.class)
-	public ResponseEntity<ApiResponse<Void>> passwordNotMatchException(PasswordNotMatchException e) {
-
-		ErrorCode errorCode = e.getErrorCode();
-		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
-
-		return ResponseEntity.status(errorCode.getHttpStatusCode())
-			.body(response);
-	}
-
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ApiResponse<Void>> resourceNotFoundException(ResourceNotFoundException e) {
-
-		ErrorCode errorCode = e.getErrorCode();
-		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
-
-		return ResponseEntity.status(errorCode.getHttpStatusCode())
-			.body(response);
-	}
-
-	@ExceptionHandler(UnAuthenticatedException.class)
-	public ResponseEntity<ApiResponse<Void>> unAuthenticatedException(UnAuthenticatedException e) {
 
 		ErrorCode errorCode = e.getErrorCode();
 		ApiResponse<Void> response = ApiResponse.ofFail(errorCode.getCode(), e.getClientMessage());
