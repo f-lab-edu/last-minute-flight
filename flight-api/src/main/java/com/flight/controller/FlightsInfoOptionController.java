0bc03db362681eda4eb33d9dcf5c9ada72023c41
guswls78963210 <guswls78963210@gmail.com>
2024-07-02 19:51:27 +0900

    [Refactor - #7] api 경로 공통 설정 추가 및 경로 수정
    추가로, 변수로 선언하여 메서드의 리턴 값을 받아 다시 메서드의 매개변수로 넘기는 구조를
    바로 매개변수로 넘기도록 수정

--- flight-api/src/main/java/com/flight/controller/FlightsInfoOptionController.java
+++ flight-api/src/main/java/com/flight/controller/FlightsInfoOptionController.java
@@ -6,10 +6,10 @@ import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.flight.application.dto.FlightsInfoOptionApplicationResponse;
 import com.flight.application.service.FlightsInfoOptionApplicationService;
 import com.flight.dto.ApiResponse;
 import com.flight.dto.FlightsInfoOptionApiResponse;
@@ -19,23 +19,23 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/flightsInfo")
 public class FlightsInfoOptionController {
 
 	private final FlightsInfoOptionApplicationService flightsInfoOptionApplicationService;
 	private final FlightsInfoOptionApiMapper mapper;
 
-	@GetMapping("/flightsInfo")
+	@GetMapping("/search")
 	public ResponseEntity<ApiResponse<List<FlightsInfoOptionApiResponse>>> searchFlightsInfoOption(
 		@RequestParam("departures") String departures,
 		@RequestParam("arrivals") String arrivals,
 		@RequestParam("departureDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate departureDate
 	) {
 
-		List<FlightsInfoOptionApplicationResponse> applicationResponses = flightsInfoOptionApplicationService.searchFlightsInfoOptions(
-			departures, arrivals, departureDate);
-
 		List<FlightsInfoOptionApiResponse> apiResponses = mapper.applicationResponseToApiResponse(
-			applicationResponses);
+			flightsInfoOptionApplicationService.searchFlightsInfoOptions(
+				departures, arrivals, departureDate)
+		);
 
 		ApiResponse<List<FlightsInfoOptionApiResponse>> response = ApiResponse.ofSuccess(apiResponses);
 
