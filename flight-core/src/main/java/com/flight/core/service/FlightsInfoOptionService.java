96a1d365746043fdd1c58f1c070dda4861b6923c
guswls78963210 <guswls78963210@gmail.com>
2024-07-09 01:35:44 +0900

    [FEAT - #9] 좌석 감소 기능과 특정 항공 정보 조회 기능 구현

--- flight-core/src/main/java/com/flight/core/service/FlightsInfoOptionService.java
+++ flight-core/src/main/java/com/flight/core/service/FlightsInfoOptionService.java
@@ -2,11 +2,17 @@ package com.flight.core.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.flight.common.exception.CheckedException;
+import com.flight.common.exception.InsufficientSeatsException;
 import com.flight.core.dto.FlightsInfoOptionDomainResponse;
-import com.flight.core.repository.FlightsInfoOptionRepository;
+import com.flight.core.repository.RedisLockManager;
+import com.flight.core.repository.flightsInfo.FlightsInfoOptionRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,9 +21,25 @@ import lombok.RequiredArgsConstructor;
 public class FlightsInfoOptionService {
 
 	private final FlightsInfoOptionRepository flightsInfoOptionRepository;
+	private final RedisLockManager lockManager;
 
 	public List<FlightsInfoOptionDomainResponse> search(String departures, String arrivals, LocalDate departureDate) {
 
 		return flightsInfoOptionRepository.search(departures, arrivals, departureDate);
+	}
+
+	public Optional<FlightsInfoOptionDomainResponse> getFlightsInfoOptionById(Long flightsInfoOptionId) {
+
+		return flightsInfoOptionRepository.findFlightsInfoById(flightsInfoOptionId);
+	}
+
+	@Transactional(rollbackFor = CheckedException.class)
+	public void decreaseSeats(Long flightsInfoOptionId, int seatCount) throws CheckedException {
+
+		if (!lockManager.executeWithLock("flightsSeatLock:" + flightsInfoOptionId, 3, 1, TimeUnit.SECONDS, () ->
+			flightsInfoOptionRepository.decreaseSeatsInAvailable(flightsInfoOptionId, seatCount) > 0
+		)) {
+			throw new InsufficientSeatsException(flightsInfoOptionId);
+		}
 	}
 }
